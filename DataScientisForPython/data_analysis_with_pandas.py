69570796a369f3ce905886288a431b988a4c5ce0
Berna UZUNOĞLU <bernauzunoglu58@gmail.com>
2024-05-19 14:53:39 +0300

    Pivot table, apply ,lambda, join operatios added.

--- DataScientisForPython/data_analysis_with_pandas.py
+++ DataScientisForPython/data_analysis_with_pandas.py
@@ -21,7 +21,8 @@ s.tail(3)
 ######################################
 # VERİ OKUMA
 
-df = pd.read_csv("DataScientisForPython/datasets/nba.csv")
+# df = pd.read_csv("DataScientisForPython/datasets/nba.csv")
+df = pd.read_csv("C:/Users/BERNA/OneDrive/Masaüstü/Data_Scientist_and_AI/DataScientisForPython/datasets/nba.csv")
 df.head()
 # pandas cheatsheet
 
@@ -38,8 +39,8 @@ df.index
 df.describe().T  #  DataFrame'in sayısal sütunlarına ilişkin istatistiksel bir özet sağlar
 df.isnull().values.any()  # DF values herhangi birinde bir tane dahi olsun boş değer var mı?
 df.isnull().sum()  # Değişkenlerin eksiklik verileri
-df["Position"].head()
-df["Position"].value_counts()  # Kategorik verilerden kaç adet var?
+df["sex"].head()
+df["sex"].value_counts()  # Kategorik verilerden kaç adet var?
 
 #############################################
 # Pandas'ta Seçim İşlemleri (Selection in Pandas)
@@ -201,3 +202,120 @@ df.groupby(["sex", "embark_town", "class"]).agg({
     "age": "mean",
     "survived": "mean",
     "sex": "count"})
+
+###########################
+# Pivot Table
+df = sns.load_dataset("titanic")
+df.head()
+
+df.pivot_table("survived", "sex","embarked")  # Pivot table ön tanımlı fonk. mean dir.
+
+# ÇIKTI
+# embarked         C         Q         S
+# sex
+# female    0.876712  0.750000  0.689655
+# male      0.305263  0.073171  0.174603
+
+df.pivot_table("survived", "sex","embarked", aggfunc="std")
+df.pivot_table("survived", "sex",["embarked", "class"])
+
+# hem cinsiyet kırılım hem gemiiye binme hem yaş kırılımmı olsun. Fakat yaş kategorik bir değişken değil
+# Yaş değişkenini kategorik değişkene çevir. cut ve qcut fonk. ile
+# eğer değişkenleri tanıyorum kategoriliye biliyorsam cut , bilemiyorsam çeyreklere göre qcut kullanılır.
+df["new_age"] = pd.cut(df["age"], [0, 10, 18, 25, 40, 90])
+df.pivot_table("survived","sex","new_age")
+df.pivot_table("survived","sex",["new_age", "class"])
+
+pd.set_option("display.width", 500)
+
+#######################
+# Apply ve Lambda
+# Apply : Satır yada sutunlarda otomatik olarak fonk. çalıştırma imkanı sağlar.
+# Lambda : Fonk tanımlama şekli kullan at fonk kullanma imkanı sağlar.
+######################
+
+
+df = sns.load_dataset("titanic")
+df.head()
+
+df["age2"] = df["age"]*2
+df["age3"] = df["age"]*5
+
+(df["age"]/10).head()
+(df["age2"]/10).head()
+(df["age3"]/10).head()
+
+for col in df.columns:
+    if "age" in col:
+        print(col)
+
+for col in df.columns:
+    if "age" in col:
+        print((df[col]/10).head())
+
+for col in df.columns:
+    if "age" in col:
+        df[col] = df[col]/10
+
+df.head()
+
+# Yukardaki işlemleri apply ve lambda ile yapalım.
+df[["age", "age2", "age3"]].apply(lambda x: x/2).head()
+
+df.loc[:, df.columns.str.contains("age")].apply(lambda x: x/2).head()
+
+# Sütunlarda age olanları bul age std hesapla
+df.loc[:, df.columns.str.contains("age")].apply(lambda x: (x - x.mean()) / x.std()).head()
+
+
+def standart_scaler(col_name):
+    return (col_name - col_name.mean()) / col_name.std()
+
+df.loc[:, df.columns.str.contains("age")].apply(standart_scaler).head()
+
+# Yapılan işlemleri yansıtmak
+df.loc[:, ["age", "age2", "age3"]] = df.loc[:, df.columns.str.contains("age")].apply(standart_scaler)
+
+# Otomatik seçim örnek
+df.loc[:, df.columns.str.contains("age")] = df.loc[:, df.columns.str.contains("age")].apply(standart_scaler)
+df.head()
+
+########################
+# Birleştirme(Join) İşlemleri
+
+import numpy as np
+m = np.random.randint(1, 30, size=(5, 3))
+df1 = pd.DataFrame(m, columns=["var1", "var2", "var3"])
+df2 = df1 + 99
+
+# Concat fonk. axis=0 tanımlı olduğu için DF'leri alt alta birleştirme yapar
+pd.concat([df1, df2])  # Bu şekilde birleştirme işlemi yapıca index bilgilerini tutuyor. indexlerler beraber alt alta olduğu gibi birleştirme işlemi yapıyor.
+pd.concat([df1, df2],ignore_index=True)  #Bu şekilde index bilgisini sıralı ilerlettik.
+
+######################
+# Merge ile Birleştirme İşlemleri
+
+df3 = pd.DataFrame({'employees': ['john', 'dennis', 'mark', 'maria'],
+                    'groups': ['accounting', 'engineering', 'engineering', 'hr']})
+
+df4 = pd.DataFrame({'employees': ['john', 'dennis', 'mark', 'maria'],
+                    'start_date': [2010, 2009, 2014, 2019]})
+
+pd.merge(df3, df4)
+pd.merge(df3, df4, on="employees")
+
+# Amaç : Her çalışanın müdürünün bilgisine erişmek istiyoruz.
+df5 = pd.merge(df3, df4)
+
+df6 = pd.DataFrame({'groups': ['accounting', 'engineering', 'hr'],
+                    'manager': ['Caner', 'Mustafa', 'Berkcan']})
+pd.merge(df5, df6)
+
+
+data = {'A': [1, 2, 3], 'B': [4, 5, 6], 'C': [7, 8, 9]}
+df = pd.DataFrame(data)
+subset = df.loc[0:1, ['A', 'B']]
+subset3 = df.loc[:, ['A', 'B']]
+
+subset1 = df.iloc[0:2, [0, 1]]
+subset2 = df.iloc[:, [0, 1]]
