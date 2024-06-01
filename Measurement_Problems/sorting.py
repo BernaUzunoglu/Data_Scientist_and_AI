1329644db505ad50577d7f40cce54280af360ef3
Berna UZUNOĞLU <bernauzunoglu58@gmail.com>
2024-06-01 18:36:26 +0300

    Measurement problems sorting. Course sorting application added.

---
+++ Measurement_Problems/sorting.py
@@ -0,0 +1,142 @@
+###################################################
+# Sorting Products
+###################################################
+
+###################################################
+# Uygulama: Kurs Sıralama
+###################################################
+import pandas as pd
+import math
+import scipy.stats as st
+from sklearn.preprocessing import MinMaxScaler
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.expand_frame_repr', False)
+pd.set_option('display.float_format', lambda x: '%.5f' % x)
+
+df = pd.read_csv("Measurement_Problems/datasets/product_sorting.csv")
+print(df.shape)
+df.head(10)
+
+####################
+# Sorting by Rating
+####################
+
+df.sort_values("rating", ascending=False).head(20)
+df.head(8)
+####################
+# Sorting by Comment Count or Purchase Count
+####################
+
+df.sort_values("purchase_count", ascending=False).head(20)
+df.sort_values("commment_count", ascending=False).head(20)
+
+####################
+# Sorting by Rating, Comment and Purchase
+####################
+# Yorum ve satın alma oranlarının sayısının daha anlamlı olabilmesi için belirli biir scala içerisine almamız daha iyi olacaktır.
+# Min max scaler ile verilen değişkeni 1-5 arasında fit ediyor ve trasnform metodu ile yeni değerleri yazıyor.
+df["purchase_count_scaled"] = MinMaxScaler(feature_range=(1, 5)). \
+    fit(df[["purchase_count"]]). \
+    transform(df[["purchase_count"]])
+
+df.describe().T
+
+df["comment_count_scaled"] = MinMaxScaler(feature_range=(1, 5)). \
+    fit(df[["commment_count"]]). \
+    transform(df[["commment_count"]])
+
+(df["comment_count_scaled"] * 32 / 100 +
+ df["purchase_count_scaled"] * 26 / 100 +
+ df["rating"] * 42 / 100)
+
+
+def weighted_sorting_score(dataframe, w1=32, w2=26, w3=42):
+    return (dataframe["comment_count_scaled"] * w1 / 100 +
+            dataframe["purchase_count_scaled"] * w2 / 100 +
+            dataframe["rating"] * w3 / 100)
+
+
+df["weighted_sorting_score"] = weighted_sorting_score(df)
+
+df.sort_values("weighted_sorting_score", ascending=False).head(20)
+
+df[df["course_name"].str.contains("Veri Bilimi")].sort_values("weighted_sorting_score", ascending=False).head(20)
+
+
+####################
+# Bayesian Average Rating Score
+####################
+
+# Sorting Products with 5 Star Rated - 5 Yıldızşı sistemlerde ürün sıralama
+# Sorting Products According to Distribution of 5 Star Rating - 5 Yıldızın dağılımına göre ürün sıralama
+
+
+#  Bayesian Average Rating Score ne iş yapar : Puan dağılımları üzerinden ağırlıklı bir şekilde olasılıksal ortalama hesabı yapar.
+#  confidence : hesaplamada elde edilecek z tablosu için bir sabit değeri temsil eder.
+def bayesian_average_rating(n, confidence=0.95):
+    if sum(n) == 0:
+        return 0
+    K = len(n)
+    z = st.norm.ppf(1 - (1 - confidence) / 2)
+    N = sum(n)
+    first_part = 0.0
+    second_part = 0.0
+    for k, n_k in enumerate(n):
+        first_part += (k + 1) * (n.iloc[k] + 1) / (N + K)
+        second_part += (k + 1) * (k + 1) * (n.iloc[k] + 1) / (N + K)
+    score = first_part - z * math.sqrt((second_part - first_part * first_part) / (N + K + 1))
+    return score
+
+
+df.head()
+
+df["bar_score"] = df.apply(lambda x: bayesian_average_rating(x[["1_point",
+                                                                "2_point",
+                                                                "3_point",
+                                                                "4_point",
+                                                                "5_point"]]), axis=1)
+
+df.sort_values("weighted_sorting_score", ascending=False).head(20)
+df.sort_values("bar_score", ascending=False).head(20)
+
+df[df["course_name"].index.isin([5, 1])].sort_values("bar_score", ascending=False)
+
+
+####################
+# Hybrid Sorting: BAR Score + Diğer Faktorler
+####################
+
+# Rating Products
+# - Average
+# - Time-Based Weighted Average
+# - User-Based Weighted Average
+# - Weighted Rating
+# - Bayesian Average Rating Score
+
+# Sorting Products
+# - Sorting by Rating
+# - Sorting by Comment Count or Purchase Count
+# - Sorting by Rating, Comment and Purchase
+# - Sorting by Bayesian Average Rating Score (Sorting Products with 5 Star Rated)
+# - Hybrid Sorting: BAR Score + Diğer Faktorler
+
+
+def hybrid_sorting_score(dataframe, bar_w=60, wss_w=40):
+    bar_score = dataframe.apply(lambda x: bayesian_average_rating(x[["1_point",
+                                                                     "2_point",
+                                                                     "3_point",
+                                                                     "4_point",
+                                                                     "5_point"]]), axis=1)
+    wss_score = weighted_sorting_score(dataframe)
+
+    return bar_score*bar_w/100 + wss_score*wss_w/100
+
+
+df["hybrid_sorting_score"] = hybrid_sorting_score(df)
+
+df.sort_values("hybrid_sorting_score", ascending=False).head(20)
+
+df[df["course_name"].str.contains("Veri Bilimi")].sort_values("hybrid_sorting_score", ascending=False).head(20)
+
